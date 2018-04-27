a4b3c88adf80a20cc51916b12557bf76a3992b70
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-26 21:21:56 -0400

    more changes

--- oop_starter.js
+++ oop_starter.js
@@ -12,6 +12,9 @@ And you can access the individual properties like:
 > myCard.suit
 'diamonds' */
 
+// Version 1
+// this is a class - the constructor and methods
+
 
 function Card(suit, number) {
   this.suit = suit;
@@ -30,5 +33,53 @@ The method should return the URL path to the png image file for the card.
 
 > myCard.getImageUrl()
 'images/5_of_diamonds.png' */
+
+
+// Version 2 - style of ES6
+// it's good practice not to add functions to the contructor so it doesn't add it to every instance of 
+// the constructor 
+
+
+class Paper() {
+	constructor(thickness){
+		this.thickness = thickness;
+	}
+}
+
+// the super class to Card is Paper, must always use super with extends
+// super is pointing to Paper and we are passing 0.5 as the parameter for Paper
+
+class Card extends Paper {  // extends will add the properties of Paper to Card
+	constructor(suit, number) {
+		super(0.5); // pointing to the constructor of the class, add before 'this'
+		this.suit = suit;
+  		this.number = number;
+	}
+}
+
+getImageUrl() {   // no longer need to add .prototype
+	return 'cards/' + this.number + this.suit + '.png';
+}
+
+
+get suit() {
+	return 'my suit is  ' + this.suit
+}
+
+set suit() {
+
+}
+
+var Card = new Card('h', 3);
+
+card.suit = 'c';
+
+
+
+
+
+
+
+
 
 
